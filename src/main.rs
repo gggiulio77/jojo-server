95eb86c1971f95a4aa0b95f8438fb36bd2169c63
ggPersonal <gggiulio77@gmail.com>
2023-06-30 18:50:51 -0300

    feat: refactor main to consume mouse and handler modules

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,20 @@
-use futures_util::{FutureExt, StreamExt};
-use warp::{ws::WebSocket, Filter};
+pub mod handler;
+pub mod mouse;
+
+use crate::mouse::Mouse;
+
+use handler::room;
+use warp::Filter;
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
+    let mouse_filter = warp::any().map(|| Mouse::default());
 
     let routes = warp::path("room")
-        // The `ws()` filter will prepare the Websocket handshake.
         .and(warp::ws())
-        .map(|ws: warp::ws::Ws| ws.on_upgrade(move |socket| handler(socket)));
+        .and(mouse_filter)
+        .map(|ws: warp::ws::Ws, mouse| ws.on_upgrade(move |socket| room(socket, mouse)));
 
     warp::serve(routes).run(([192, 168, 0, 163], 3030)).await;
-}
-
-async fn handler(ws: WebSocket) {
-    let (mut _tx, mut rx) = ws.split();
-
-    while let Some(result) = rx.next().await {
-        let msg = match result {
-            Ok(msg) => msg,
-            Err(e) => {
-                eprintln!("websocket error: {}", e);
-                break;
-            }
-        };
-
-        println!("[msg]: {:?}", msg.to_str().unwrap());
-    }
 }
