3384376f0c483286f145b0c3ba2954b6cd977f52
ggPersonal <gggiulio77@gmail.com>
2023-06-30 19:05:16 -0300

    feat: add dotenv and log dependencies

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,15 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
+    dotenv::dotenv().ok();
     pretty_env_logger::init();
+
     let mouse_filter = warp::any().map(|| Mouse::default());
 
     let routes = warp::path("room")
         .and(warp::ws())
         .and(mouse_filter)
-        .map(|ws: warp::ws::Ws, mouse| ws.on_upgrade(move |socket| room(socket, mouse)));
+        .map(|ws: warp::ws::Ws, mouse| ws.on_upgrade(|socket| room(socket, mouse)));
 
     warp::serve(routes).run(([192, 168, 0, 163], 3030)).await;
 }
