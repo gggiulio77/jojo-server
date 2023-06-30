61b6c74c2bd72f04443e96dd762d13d32996d6e1
ggPersonal <gggiulio77@gmail.com>
2023-06-30 18:50:21 -0300

    feat: create mouse module

---
+++ src/mouse.rs
@@ -0,0 +1,52 @@
+use enigo::*;
+use serde::{Deserialize, Serialize};
+
+pub trait MouseDriver {
+    fn mouse_move_relative(&mut self, x: i32, y: i32);
+}
+
+impl MouseDriver for Enigo {
+    fn mouse_move_relative(&mut self, x: i32, y: i32) {
+        MouseControllable::mouse_move_relative(self, x, y)
+    }
+}
+
+// TODO: find a way to derive Copy/Clone
+#[derive()]
+pub struct Mouse {
+    pub x_sen: u8,
+    pub y_sen: u8,
+    // TODO: make this private
+    pub driver: Box<dyn MouseDriver + Send>,
+}
+
+impl Default for Mouse {
+    fn default() -> Mouse {
+        Mouse {
+            x_sen: 1,
+            y_sen: 1,
+            driver: Box::new(Enigo::new()),
+        }
+    }
+}
+
+impl Mouse {
+    pub fn new(x_sen: u8, y_sen: u8, driver: Box<dyn MouseDriver + Send>) -> Mouse {
+        Mouse {
+            x_sen,
+            y_sen,
+            driver,
+        }
+    }
+
+    // TODO: find a way to implement this (don't know why it does not work)
+    // pub fn mouse_move_relative(mut self, x: i32, y: i32) {
+    //     self.driver.mouse_move_relative(x, y)
+    // }
+}
+
+#[derive(Clone, Copy, Debug, Deserialize, Serialize)]
+pub struct MouseRead {
+    pub x_read: i32,
+    pub y_read: i32,
+}
