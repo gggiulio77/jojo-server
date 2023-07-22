4c494ec88242caeb64d195e23370b32908f95795
ggPersonal <gggiulio77@gmail.com>
2023-07-22 20:14:34 -0300

    feat: add broadcast mod and get local ip

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,37 @@
+pub mod discovery;
 pub mod handler;
 pub mod mouse;
 
 use crate::mouse::Mouse;
 
 use handler::multiple;
+use local_ip_address::local_ip;
+use log::info;
+use std::net::SocketAddr;
 use warp::Filter;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     dotenv::dotenv().ok();
     pretty_env_logger::init();
+
+    let bind_address: SocketAddr = match std::env::var("BROADCAST_BIND_ADDRESS") {
+        Ok(value) => value.parse::<SocketAddr>().unwrap(),
+        Err(_) => {
+            panic!("BROADCAST_BIND_ADDRESS env not found")
+        }
+    };
+
+    let my_local_ip = match local_ip()? {
+        std::net::IpAddr::V4(ip) => ip,
+        _ => {
+            panic!("IpV4 not found")
+        }
+    };
+
+    info!("local ip: {:?}", my_local_ip);
+
+    discovery::init_broadcast(bind_address).await;
 
     let mouse_filter = warp::any().map(|| Mouse::default());
 
@@ -18,7 +40,7 @@ async fn main() {
         .and(mouse_filter)
         .map(|ws: warp::ws::Ws, mouse| ws.on_upgrade(|socket| multiple(socket, mouse)));
 
-    warp::serve(multiple_route)
-        .run(([192, 168, 0, 163], 3030))
-        .await;
+    warp::serve(multiple_route).run((my_local_ip, 3030)).await;
+
+    Ok(())
 }
